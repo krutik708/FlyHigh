f359dfc1b05452c0a11774604fb688d6fc6a2a82
SiddharthRevankar <113912060+SiddharthRevankar@users.noreply.github.com>
2022-10-30 01:25:03 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 
 Route::view('home', 'homepage');
 Route::view('flights', 'availableflights');
+Route::view('/form','form');
 Route::prefix('google')->name('google.')->group( function(){
     Route::get('login', [GoogleController::class, 'loginWithGoogle'])->name('login');
     Route::any('callback', [GoogleController::class, 'callbackFromGoogle'])->name('callback');
